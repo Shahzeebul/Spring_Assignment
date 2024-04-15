527da09dbca5dee7e1da36e75d13f2f55118cd74
Shahzaib Hoda <104351775+Shahzeebul@users.noreply.github.com>
2024-04-16 01:41:06 +0530

    Update User.java

--- Spring_Assignment/src/main/java/shaz/User.java
+++ Spring_Assignment/src/main/java/shaz/User.java
@@ -4,13 +4,13 @@ public class User {
     private String username;
     private String password;
     private String name;
-    private String course; // Changed from 'bio'
+    private String course;
 
-    public User(String user, String pass, String newName, String newCourse) { // Changed from 'new_name' and 'new_bio'
+    public User(String user, String pass, String newName, String newCourse) { 
         username = user;
         password = pass;
         name = newName;
-        course = newCourse; // Changed from 'new_bio'
+        course = newCourse; 
     }
 
     public String getUsername() {
@@ -37,11 +37,11 @@ public class User {
         this.name = name;
     }
 
-    public String getCourse() { // Changed from 'getBio'
+    public String getCourse() {
         return course;
     }
 
-    public void setCourse(String course) { // Changed from 'setBio'
+    public void setCourse(String course) {
         this.course = course;
     }
 
@@ -49,6 +49,6 @@ public class User {
         System.out.println("Username: " + username);
         System.out.println("Password: " + password);
         System.out.println("Name: " + name);
-        System.out.println("Course: " + course); // Changed from 'Bio'
+        System.out.println("Course: " + course); 
     }
 }
